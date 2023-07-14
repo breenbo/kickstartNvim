5d2ac404abd8c6f79d21ca55022fcbcad238e1f8
Bruno Berrehuel <bruno@homepad.ch>
2023-07-14 17:35:29 +0200

    Add leap/lualine/surround

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -8,23 +8,22 @@ return {
       theme = "tokyonight",
       component_separators = "|",
       section_separators = "",
+      globalstatus = true,
     },
     sections = {
       lualine_a = { "mode" },
       lualine_b = { "branch", "diff" },
-      lualine_c = { "filename", "diagnostics" },
+      lualine_c = { "diagnostics" },
       lualine_x = { "filetype" },
       lualine_y = { "progress" },
       lualine_z = { "location" },
     },
-    -- winbar = {
-    --   lualine_a = { "buffers" },
-    --   lualine_b = {},
-    --   lualine_c = {},
-    --   lualine_x = {},
-    --   lualine_y = {},
-    --   lualine_z = {},
-    -- },
+    winbar = {
+      lualine_a = { "#%t# %m" },
+    },
+    inactive_winbar = {
+      lualine_a = { "%f" },
+    },
     extensions = {
       "neo-tree",
       "toggleterm",
